bed6226c14f32e0479f4a39b386e36a857fe5074
Shaydro <shawntalley88@gmail.com>
2023-07-08 16:20:42 -0500

    Finished Etch-A-Sketch

---
+++ etch-a-sketch/scripts.js
@@ -0,0 +1,52 @@
+let color = "black";
+let click = true;
+
+function populateBoard(size) {
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+  board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+
+  let amount = size * size;
+  for (let i = 0; i < amount; i++) {
+    let square = document.createElement("div");
+    square.addEventListener("mouseover", colorSquare);
+    square.style.backgroundColor = "LightGray";
+    board.appendChild(square);
+  }
+}
+
+populateBoard(16);
+
+function changeSize(input) {
+  if (input >= 2 && input <= 100) {
+    populateBoard(input);
+  } else {
+    alert("Too many squares!,please enter a value less than 100");
+  }
+}
+
+function colorSquare() {
+  if (click) {
+    if (color === "random") {
+      this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%`;
+    } else {
+      this.style.backgroundColor = color;
+    }
+  }
+}
+
+function changeColor(choice) {
+  color = choice;
+}
+
+function resetBoard() {
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => (div.style.backgroundColor = "LightGray"));
+}
+
+document.querySelector("body").addEventListener("click", () => {
+  click = !click;
+});
